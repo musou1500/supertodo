3247e4b8515ea932030a3a48790b092547798581
musou1500 <musou1500@gmail.com>
2018-11-30 18:15:48 +0900

    add users routes

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const bearerToken = require("express-bearer-token");
 const tasksRouter = require("./routes/tasks");
 const authRouter = require("./routes/auth");
 const tagsRouter = require("./routes/tags");
+const usersRouter = require("./routes/users");
 
 var app = express();
 
@@ -17,5 +18,6 @@ app.use(cookieParser());
 app.use("/tasks", tasksRouter);
 app.use("/auth", authRouter);
 app.use("/tags", tagsRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,50 @@
-var express = require('express');
-var router = express.Router();
+const models = require("../models");
+const express = require("express");
+const router = express.Router();
+const auth = require("../middlewares/auth");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.param("screenId", (req, res, next, screenId) => {
+  const user = await models.User.findOne({ where: { screenId } });
+
+  if (user === undefined) {
+    res.status(404).send({
+      error: "no such user"
+    });
+    return;
+  }
+
+  req.user = user;
+  next();
+});
+
+router.use(auth());
+
+router.post("/", async (req, res) => {
+  const task = await models.Task.create(
+    {
+      assigneeId: req.body.assigneeId,
+      authorId: req.body.authorId,
+      name: req.body.name,
+      tags: req.body.tags
+    },
+    {
+      include: [
+        models.Tag,
+        { model: models.User, as: "author" },
+        { model: models.User, as: "assignee" }
+      ]
+    }
+  );
+
+  res.send(task);
+});
+
+router.get("/:screenId", async (req, res) => res.send(req.user));
+router.get("/me", async (req, res) => {
+  const user = await models.User.findOne({
+    where: { screenId: req.me.screenId }
+  });
+  res.send(user);
 });
 
 module.exports = router;
