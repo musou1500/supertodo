b2cdeaa775fab204cefab4587f4de62be4eded2f
musou1500 <musou1500@gmail.com>
2018-11-30 18:52:40 +0900

    make password field hidden

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,18 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
-    name: DataTypes.STRING,
-    screenId: DataTypes.STRING,
-    password: DataTypes.STRING
-  });
+  const User = sequelize.define(
+    "User",
+    {
+      name: DataTypes.STRING,
+      screenId: DataTypes.STRING,
+      password: DataTypes.STRING
+    },
+    {
+      defaultScope: {
+        attributes: { exclude: ["password"] }
+      }
+    }
+  );
   User.associate = function(models) {
     // associations can be defined here
   };
